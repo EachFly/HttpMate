ad2f68d29e9a34da29f49927d38f03ce60a98ff9
GitHub Action <action@github.com>
2025-11-21 14:43:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/jeraxxxxxxx/httpmate/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/jeraxxxxxxx/httpmate/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.jeraxxxxxxx.httpmate.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
