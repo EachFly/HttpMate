7963c9a91b79ae74b40ab70fee85d2e7bf0fcb1d
Ricky <44646571+JerAxxxxxxx@users.noreply.github.com>
2025-12-19 21:36:48 +0800

    test: Fix failing unit tests and restore test stability

--- src/test/kotlin/com/github/jeraxxxxxxx/httpmate/MyPluginTest.kt
+++ src/test/kotlin/com/github/jeraxxxxxxx/httpmate/MyPluginTest.kt
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import com.github.jeraxxxxxxx.httpmate.services.MyProjectService
+import com.github.jeraxxxxxxx.httpmate.services.HttpMateProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
@@ -30,9 +30,24 @@ class MyPluginTest : BasePlatformTestCase() {
     }
 
     fun testProjectService() {
-        val projectService = project.service<MyProjectService>()
+        val projectService = project.service<HttpMateProjectService>()
 
-        assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
+        // Test service initialization
+        assertNotNull(projectService)
+        
+        // Test default configuration
+        assertEquals("http-mate/docs", projectService.docOutputDir)
+        assertEquals(false, projectService.autoOpenAfterGeneration)
+        assertEquals(0, projectService.totalDocsGenerated)
+        
+        // Test configuration modification
+        projectService.docOutputDir = "custom/docs"
+        assertEquals("custom/docs", projectService.docOutputDir)
+        
+        // Test generation recording
+        projectService.recordGeneration("test.md")
+        assertEquals(1, projectService.totalDocsGenerated)
+        assertTrue(projectService.getGeneratedFiles().contains("test.md"))
     }
 
     override fun getTestDataPath() = "src/test/testData/rename"
